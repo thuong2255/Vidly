5cc8c1d864afed4b6e9685909fe5ac9e866ab433
thuong <thuong@DESKTOP-0O3BI5A>
2017-06-02 17:21:38 +0700

    complete validation form

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using Vidly.Validation;
 
 namespace Vidly.Models
 {
@@ -7,15 +8,21 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter customers'name")]
         [StringLength(255)]
         public string Name { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? BirthDate { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
+
+
         [Display(Name = "Membership Type")]
+        [Required]
         public MemberShipType MemberShipType { get; set; }
+
         public byte MemberShipTypeId { get; set; }
     }
 }
